c1b6a793a8d5144aa30934ea49b682b0af187a09
stefanopaulo <stefanopaulo47@gmail.com>
2020-10-24 14:19:27 -0300

    processamento de dados em java

---
+++ processamento_de_dados/src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		int a, b;
+		double result;
+		
+		a = 5;
+		b = 2;
+		
+		result = (double) a / b;
+		
+		System.out.println(result);
+	}
+
+}
